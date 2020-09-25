fa6de864881d6ace195ef684cf5dac1d6b613d93
Afonso Machado <afonso777@hotmail.com>
2020-09-25 15:01:08 -0300

    estrutura inicial do mobile header

--- src/components/MobileHeader/index.tsx
+++ src/components/MobileHeader/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, ProfileCircle, SearchInput, MessageIcon } from './styles';
 
 const MobileHeader: React.FC = () => {
   return (
     <Container>
-      <h1>MobileHeader</h1>
+      <ProfileCircle src="https://avatars3.githubusercontent.com/u/11397955?s=460&u=e11b0f50547f9a3b9be9e8b076b652f374ff46b8&v=4" />
+      <SearchInput placeholder="Pesquisar" />
+      <MeassageIcon />
     </Container>
   );
 };
--- src/components/MobileHeader/styles.ts
+++ src/components/MobileHeader/styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.div``;
 
-`;
+export const ProfileCircle = styled.div``;
+
+export const SearchInput = styled.div``;
+
+export const MessageIcon = styled.div``;
