4c79858a6c79201f284d3114d0f10c53af4e7cd7
Afonso Machado <afonso777@hotmail.com>
2020-09-25 15:12:48 -0300

    implementado mobile header

--- src/components/MobileHeader/index.tsx
+++ src/components/MobileHeader/index.tsx
@@ -7,7 +7,7 @@ const MobileHeader: React.FC = () => {
     <Container>
       <ProfileCircle src="https://avatars3.githubusercontent.com/u/11397955?s=460&u=e11b0f50547f9a3b9be9e8b076b652f374ff46b8&v=4" />
       <SearchInput placeholder="Pesquisar" />
-      <MeassageIcon />
+      <MessageIcon />
     </Container>
   );
 };
--- src/components/MobileHeader/styles.ts
+++ src/components/MobileHeader/styles.ts
@@ -1,9 +1,61 @@
 import styled from 'styled-components';
+import { FiMessageSquare } from 'react-icons/fi';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  background: var(--color-link);
+  padding: 0 16px;
 
-export const ProfileCircle = styled.div``;
+  height: 48px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
-export const SearchInput = styled.div``;
+  /* componente header sempre fixado na tela, encontado no top, left e right do pai */
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 2;
 
-export const MessageIcon = styled.div``;
+  /* não aparece com a tela grande */
+  @media (min-width: 1280px) {
+    display: none;
+  }
+`;
+
+export const ProfileCircle = styled.img`
+  width: 28px;
+  height: 28px;
+
+  border-radius: 50%;
+  border: 1px solid var(--color-icons);
+`;
+
+export const SearchInput = styled.input`
+  margin-left: 16px;
+  width: 100%;
+
+  background: var(--color-input);
+  color: var(--color-black);
+  font-size: 14px;
+  padding: 7.5px 8px;
+
+  border: none;
+  outline: none;
+
+  border-radius: 2px;
+
+  &:focus {
+    background: var(--color-white);
+  }
+`;
+
+export const MessageIcon = styled(FiMessageSquare)`
+  width: 24px;
+  height: 24px;
+  color: var(--color-white);
+  border-radius: 4px;
+  flex-shrink: 0;
+
+  margin-left: 17px;
+`;
