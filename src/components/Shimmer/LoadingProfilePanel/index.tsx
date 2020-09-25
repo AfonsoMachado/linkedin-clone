f69406b2bd39af93ac31b9adbd86b9d4269c4352
Afonso Machado <afonso777@hotmail.com>
2020-09-25 18:46:14 -0300

    base do shimmer effect

--- src/components/Shimmer/LoadingProfilePanel/index.tsx
+++ src/components/Shimmer/LoadingProfilePanel/index.tsx
@@ -9,13 +9,12 @@ const LoadingProfilePanel: React.FC = () => {
   return (
     <Container>
       <Panel className="no-shadow">
-        <Skeleton className="bg-skeleton">
-          <span>
-            <Skeleton className="avatar-skeleton" />
-            <Skeleton className="row-skeleton" />
-            <Skeleton className="row-skeleton" />
-          </span>
-        </Skeleton>
+        <Skeleton className="bg-skeleton" />
+        <span>
+          <Skeleton className="avatar-skeleton" />
+          <Skeleton className="row-skeleton" />
+          <Skeleton className="row-skeleton" />
+        </span>
       </Panel>
     </Container>
   );
